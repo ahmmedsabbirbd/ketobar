571b5304ec936908f906b13060efd0c641b87e84
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-09-29 14:25:37 +0600

    header working

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@
 		/*** slick slider  */
 		$('.client-slider').slick();
 
+		/*** Search bar */
+        $('.header-search').on('click', '.search-toggle', function(e) {
+            e.preventDefault();
+            var selector = $(this).data('selector');
+            $(selector).toggleClass('show').find('.search-input').focus();
+        });
 	});
 
 }(jQuery));
