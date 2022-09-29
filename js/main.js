0ababec008d7a813a197341122170c57ec6f891c
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-09-29 17:54:04 +0600

    banner complete

--- js/main.js
+++ js/main.js
@@ -3,6 +3,45 @@
 	jQuery(document).ready(function(){
 
 
+		/*** Sticky header */
+		$(window).scroll(function(){
+			if($("body").scrollTop() > 0 || $("html").scrollTop() > 0) {
+				$(".header").addClass("stop");
+			} else {
+				$(".header").removeClass("stop");
+			}
+		});
+
+		/*** Sticky header */
+		const body = document.body;
+		const scrollUp = "scroll-up";
+		const scrollDown = "scroll-down";
+		let lastScroll = 100;
+
+		window.addEventListener("scroll", () => {
+			const currentScroll = window.pageYOffset;
+			if (currentScroll <= 0) 
+			{
+				body.classList.remove(scrollUp);
+				return;
+			}
+
+			if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) 
+			{
+				// down
+				body.classList.remove(scrollUp);
+				body.classList.add(scrollDown);
+			} 
+			else if ( currentScroll < lastScroll && body.classList.contains(scrollDown) ) 
+			{
+				// up
+				body.classList.remove(scrollDown);
+				body.classList.add(scrollUp);
+			}
+
+			lastScroll = currentScroll;
+		});
+
 		/*** mobile menu  */
 		$(".hamburger-menu").on("click", function () {
 			$(".side-info").toggleClass("info-open");
